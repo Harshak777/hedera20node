bfadd953f71da34e1c7bcf9a5525d22579064f9b
Harshak Krishnaa <33751325+Harshak777@users.noreply.github.com>
2020-06-10 12:31:59 +0530

    Update fees.js

--- routes/fees.js
+++ routes/fees.js
@@ -48,8 +48,8 @@ router.route('/bal').post(async (req, res) => {
   router.route('/fiveh').post(async (req, res) => {
     console.log(req.body.message)
     receipt =await (await new CryptoTransferTransaction()
-    .addSender('0.0.49451', 500000000000)
-    .addRecipient(req.body.account, 500000000000)
+    .addSender('0.0.49451', 50000000000)
+    .addRecipient(req.body.account, 50000000000)
     .build(HederaClient)
     .execute(HederaClient))
     .getReceipt(HederaClient);
@@ -60,8 +60,8 @@ router.route('/bal').post(async (req, res) => {
   router.route('/tenh').post(async (req, res) => {
     console.log(req.body.message)
     receipt =await (await new CryptoTransferTransaction()
-    .addSender('0.0.49451', 1000000000000)
-    .addRecipient(req.body.account, 1000000000000)
+    .addSender('0.0.49451', 100000000000)
+    .addRecipient(req.body.account, 100000000000)
     .build(HederaClient)
     .execute(HederaClient))
     .getReceipt(HederaClient);
